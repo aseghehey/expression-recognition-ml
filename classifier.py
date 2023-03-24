1913a1da9464c0b1c4ceffcedc19bc9672baed53
easeghehey <emanuelaseghey9@gmail.com>
2023-03-24 16:59:12 -0400

    subject independent splits

--- classifier.py
+++ classifier.py
@@ -3,34 +3,42 @@ import numpy as np
 from sklearn import svm
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from FileReading import GetData
+from classify import PrintEvalMetrics
 
-def Classify(X, y, ind, nFold=10, clf="SVM"):
+def Split(dataframe, nFold=10):
+    subjects = dataframe['subject'].unique()
+    subject_folds = np.array_split(subjects, nFold)
+    
+    split_indices = []
+    for i in range(len(subject_folds)):
+        test_subjects = subject_folds[i]
+        test = dataframe[dataframe['subject'].isin(test_subjects)]
+        train = dataframe[~dataframe['subject'].isin(test_subjects)]
+        split_indices.append([train.index, test.index])
+    return split_indices
+    
+
+def Classify(df, X, nFold=10, clf="SVM"):
 
     if clf == "TREE": 
         clf = DecisionTreeClassifier()
     elif clf == "RF": 
         clf = RandomForestClassifier()
     else: 
-        clf = svm.SVC()
+        clf = svm.LinearSVC(dual=False)
+
+    y = df['target'].to_numpy()
+    split_indices = Split(df, nFold)
 
     pred = []
     test_indices = []
-
-    indexes = np.array_split(ind, nFold)
-    splits = []
-    for i in range(nFold):
-        t = []
-        for j in range(nFold):
-            if i == j: continue
-            t.extend(indexes[j])
-        splits.append((t, indexes[i]))
     
-    for i, (train_index, test_index) in enumerate(splits):
-        start = time.perf_counter()
+    for i, (train_index, test_index) in enumerate(split_indices):
         print(f"Fold {i}")
+        start = time.perf_counter()
         clf.fit(X[train_index], y[train_index])
-        elapsed = time.perf_counter() - start
-        print(f"Time: {elapsed}")
+        print(f"Elapsed train: {time.perf_counter() - start}")
         pred.append(clf.predict(X[test_index]))
         test_indices.append(test_index)
 

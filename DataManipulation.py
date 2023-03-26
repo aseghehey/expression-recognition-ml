f48b2c2a70b1c24b7e3abfe1f35b7626a11452f1
easeghehey <emanuelaseghey9@gmail.com>
2023-03-26 17:20:03 -0400

    fixed bug on translate

--- DataManipulation.py
+++ DataManipulation.py
@@ -1,26 +1,46 @@
-import math
 import numpy as np
 
+DIM = 3
 def Translate(arr):
-    average = sum(arr)/len(arr)
-    for i in range(len(arr)):
-        arr[i] -= average
-    return arr
+    X_sum = 0
+    Y_sum = 0
+    Z_sum = 0
+    total = len(arr)/DIM
+    get_it = arr[:DIM]
+    # not executing:
+    for i in range(0, len(arr), DIM):
+        X_sum += arr[i]
+        Y_sum += arr[i + 1]
+        Z_sum += arr[i + 2]
+
+    X_avg = X_sum / total
+    Y_avg = Y_sum / total
+    Z_avg = Z_sum / total
+
+    for i in range(0, len(arr), DIM):
+        arr[i] -= X_avg
+        arr[i + 1] -= Y_avg
+        arr[i + 2] -= Z_avg
 
 def Rotate(arr, idx) -> list:
-    """ idx {0:x, 1:y, 2: z}"""
-    for i in range(len(arr)):
-        if i == idx: continue
-        arr[i] *= -1
-    return arr    
+    """ Given array, rotate it over the X-, Y-, or Z-axis. Use idx to specify which axis to rotate over, where 0 is X, 1 is Y, and 2 is Z."""
+    for i in range(0, len(arr), DIM):
+        if (idx == 0):
+            arr[i + 1] *= -1
+            arr[i + 2] *= -1
+        elif (idx == 1):
+            arr[i] *= -1
+            arr[i + 2] *= -1
+        else:
+            arr[i] *= -1
+            arr[i + 1] *= -1
 
 def ManipulateData(data, manipulation):
-    manipulation = manipulation.lower()
-    if manipulation == "original":
-        return data
+    """ Connects the Translate Rotate functions together"""
     axis_index = {'rotatedx': 0,
                   'rotatedy': 1,
                   'rotatedz': 2}
-    if  manipulation == "translate":
-        return Translate(data)
-    return Rotate(data, axis_index[manipulation])
+    if  manipulation == "translated":
+        Translate(data)
+        return
+    Rotate(data, axis_index[manipulation])

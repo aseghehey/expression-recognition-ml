aee5778cdfa9a6103d7b46ddb6b792b4ab590519
easeghehey <emanuelaseghey9@gmail.com>
2023-03-26 19:05:31 -0400

    final results, requirements and comments

--- DataManipulation.py
+++ DataManipulation.py
@@ -2,12 +2,12 @@ import numpy as np
 
 DIM = 3
 def Translate(arr):
+    """ Given array, translate it to the origin. """
+    # Find the average of each axis
     X_sum = 0
     Y_sum = 0
     Z_sum = 0
     total = len(arr)/DIM
-    get_it = arr[:DIM]
-    # not executing:
     for i in range(0, len(arr), DIM):
         X_sum += arr[i]
         Y_sum += arr[i + 1]
@@ -17,6 +17,7 @@ def Translate(arr):
     Y_avg = Y_sum / total
     Z_avg = Z_sum / total
 
+    # Translate the data to the origin
     for i in range(0, len(arr), DIM):
         arr[i] -= X_avg
         arr[i + 1] -= Y_avg
@@ -25,13 +26,13 @@ def Translate(arr):
 def Rotate(arr, idx) -> list:
     """ Given array, rotate it over the X-, Y-, or Z-axis. Use idx to specify which axis to rotate over, where 0 is X, 1 is Y, and 2 is Z."""
     for i in range(0, len(arr), DIM):
-        if (idx == 0):
+        if (idx == 0): # Rotate over X-axis
             arr[i + 1] *= -1
             arr[i + 2] *= -1
-        elif (idx == 1):
+        elif (idx == 1): # Rotate over Y-axis
             arr[i] *= -1
             arr[i + 2] *= -1
-        else:
+        else: # Rotate over Z-axis
             arr[i] *= -1
             arr[i + 1] *= -1
 
